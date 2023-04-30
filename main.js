65ed4c83f963ebae4b3863f394588a401e223669
lazaremikaia <121046020+lazaremikaia@users.noreply.github.com>
2023-04-30 22:39:30 +0430

    changes 3

--- main.js
+++ main.js
@@ -1,26 +1,103 @@
-const body = document.body
-const div = document.createElement ('div')
-div.classList.add('container')
-body.append(div)
-div.style.maxWidth = "400px"
-div.style.backgroundColor = "grey"
+// const body = document.body
+// const div = document.createElement ('div')
+// div.classList.add('container')
+// body.append(div)
+// div.style.maxWidth = "400px"
+// div.style.backgroundColor = "grey"
 
-const title = document.createElement ('h1')
-title.innerHTML = "Programing"
-div.appendChild(title)
-title.style.textAlign = "center"
 
-const image = document.createElement ('img')
-image.src ="./programing.jpeg"
-div.appendChild(image)
-image.style.margin = "50px"
+// const title = document.createElement ('h1')
+// title.innerHTML = "Programing"
+// div.appendChild(title)
+// title.style.textAlign = "center"
 
-const text = document.createElement ('p')
-text.innerHTML ="Programming refers to a technological process for telling a computer which tasks to perform in order to solve problems. You can think of programming as a collaboration between humans and computers, in which humans create instructions for a computer to follow (code) in a language computers can understand"
-div.appendChild (text)
-text.style.color ="white"
+// const image = document.createElement ('img')
+// image.src ="./programing.jpeg"
+// div.appendChild(image)
+// image.style.margin = "50px"
 
-const button = document.createElement ('button')
-button.innerHTML = "reed more"
-div.appendChild (button)
-button.style.marginLeft = "150px"
+// const text = document.createElement ('p')
+// text.innerHTML ="Programming refers to a technological process for telling a computer which tasks to perform in order to solve problems. You can think of programming as a collaboration between humans and computers, in which humans create instructions for a computer to follow (code) in a language computers can understand"
+// div.appendChild (text)
+// text.style.color ="white"
+
+// const button = document.createElement ('button')
+// button.innerHTML = "reed more"
+// div.appendChild (button)
+// button.style.marginLeft = "150px"
+
+
+
+// const   num = (a) =>{
+//     fc = 1
+    
+// for (let i = 1; i <= a; i++) {
+//     fc *= i
+    
+// }
+//     return fc;
+    
+// }
+
+// console.log (num(5))
+
+
+
+
+
+const  num = (a,b,c) =>{
+
+for (let i = 0; i < a.length; i++) {
+    
+    
+}
+
+}
+
+
+num ([1,2,5,8], 2, 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
